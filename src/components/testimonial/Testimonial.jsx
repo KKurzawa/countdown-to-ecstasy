371e5a8f8f4b0a6d042e8aadaca4afc71cde3f25
Kris Kurzawa <kurzawa1@yahoo.com>
2025-05-20 15:04:00 -0400

    linked server i hope

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -3,8 +3,8 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import TestimonialModal from '../testimonialModal/TestimonialModal'
 
-// const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
-const apiBaseUrl = "http://localhost:3001"
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
+// const apiBaseUrl = "http://localhost:3001"
 
 const Testimonial = () => {
     const [input, setInput] = useState({
